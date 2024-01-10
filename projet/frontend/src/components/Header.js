a3919a53325fc7fb12f81406d9bf42b9af44bb03
WinnMBG <winn77.m@hotmail.com>
2024-01-10 22:17:00 +0100

    add condition

--- projet/frontend/src/components/Header.js
+++ projet/frontend/src/components/Header.js
@@ -1,30 +1,43 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-
 /**
- * 
+ *
  * Header movie component
  */
 const Header = () => {
-    return (
-       <div className="header">
-        <nav>
-            <ul>
-                <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li>Accueil</li>
-                </NavLink>
-                <NavLink to="/coup-de-coeur" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li>Coup de Coeur</li>
-                </NavLink>
-                <NavLink to="/login" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li>Se connecter</li>
-                </NavLink>
-            </ul>
-        </nav>
-        <h1>CinéDuRaT</h1>
-       </div>
-    );
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          {window.location.pathname === "/coup-de-coeur" ? (
+            <>
+              {" "}
+              <NavLink
+                to="/"
+                className={(nav) => (nav.isActive ? "nav-active" : "")}
+              >
+                <li>Accueil</li>
+              </NavLink>
+              <NavLink
+                to="/coup-de-coeur"
+                className={(nav) => (nav.isActive ? "nav-active" : "")}
+              >
+                <li>Coup de Coeur</li>
+              </NavLink>
+            </>
+          ) : null}
+          <NavLink
+            to="/login"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Se connecter</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <h1>CinéDuRaT</h1>
+    </div>
+  );
 };
 
 export default Header;
